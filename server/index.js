283b3720cbb47290093eee89e3df97e323da6c16
Anuj Singh <anujsingh21092000@gmail.com>
2024-03-17 21:34:41 +0530

    Created Models for Users, Videos and Comments

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import Express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = Express();
 const Port = process.env.PORT || 3000;
+const MongoConnect = () => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("Database Connected Successfully");
+    })
+    .catch((error) => {
+      console.log("There Was an Error connecting to database");
+      console.log(error.message);
+    });
+};
+
 app.listen(Port, () => {
+  MongoConnect();
   console.log(`Server Running on Port ${Port}`);
 });
